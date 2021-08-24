2e4016d0c8513e708f65425ddfddeff0fa4c263c
munish710 <munish710@gmail.com>
2021-08-24 23:53:02 +0530

    authenticating the user request by verifying the incoming token

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -18,11 +17,26 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
   const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Heloo, Munish Thakur`,
-    secret: `Here is theyour authenctiated data, your lucky number is ${luckyNumber}`,
-  });
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authenctiated data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new CustomAPIError("Not authorised to access this route", 401);
+  }
 };
 
 module.exports = { login, dashboard };
