b8e29210659757f0c01bceec477e2fd8746bfe00
munish710 <munish710@gmail.com>
2021-08-24 23:12:27 +0530

    setup signed jwt toekn from thebackend

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,20 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Reegister/Signup Route");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  return res.status(200).json({ msg: "user created ", token });
 };
 
 const dashboard = async (req, res) => {
